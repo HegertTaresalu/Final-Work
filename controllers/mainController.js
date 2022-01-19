cfa2f80448a831827d274b40d1efb2f8d2fb4b72
Opilane TTHK <opilane@tthk.ee>
2022-01-19 14:10:23 +0200

    add post routes

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,18 +1,61 @@
-const User = require ("../models/user")
-const passport = require("passport")
-const date = require("../getDate.js")
-
+const User = require('../models/user');
+const passport = require('passport');
+const ate = require("../getDate")
 exports.getMainPage = (req,res) =>{
     let today = date.getDate();
     res.render("index", {dateToRender:today});
 };
 
 exports.getAdminPage = (req,res) =>{
-    res.render("admin")
-
+    if(req.isAuthenticated)
+    {
+        res.render("admin")
+    }
+    else{
+        res.render("register")
+    }
 };
 
 exports.getSigninPage = (req,res) =>{
     res.render("signin");
 
+};
+exports.getRegisterPage = (req,res) => {
+    res.render("register")
+};
+
+exports.postSignIn = (req,res)=>{
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user,(error)=>{
+        if(error){
+            console.log(error)
+            res.redirect("/login")
+        }
+        else{
+            passport.authenticate("local")(req,res,()=>{
+                res.render("admin");
+            });
+        }
+    });
+};
+
+
+
+
+exports.postRegister = (req, res) => {
+    User.register({username: req.body.username}, req.body.password, (error, user)=>{
+        console.log(req.body.password)
+        if(error){
+            console.log(error);
+            res.redirect('/register');
+        } else {
+            passport.authenticate('local')(req, res, ()=> {
+                res.render('admin');
+            });
+        }
+    });
 };
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mainController = require("../controllers/mainController");
-const date = require
+//const date = require
 const router = express.Router();
 
 router.get("/signin", mainController.getSigninPage);
-
 router.get("/", mainController.getMainPage);
 
-
 router.get("/admin", mainController.getAdminPage);
+router.post("/admin",mainController.postSignIn);
 
 
-
+router.get("/register", mainController.getRegisterPage);
+router.post("/register", mainController.postRegister);
 module.exports = router
 
